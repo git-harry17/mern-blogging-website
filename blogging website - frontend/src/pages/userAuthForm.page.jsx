a87b82899f8a63c0fa5becf6017401bfc7b763ef
Bhawana Rautela <bhawanarautela06@gmail.com>
2025-05-07 23:56:47 +0530

    test3

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,10 +1,12 @@
+import AnimationWrapper from"../common/page-animation";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png" ;
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
 const UserAuthForm = ({type})=>
 {
     return (
-        <section className="h-cover flex items-center justify-center ">
+        <AnimationWrapper keyValue={type}>
+            <section className="h-cover flex items-center justify-center ">
             <form className="w-[80%] max-w-[400px]">
                 <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
                     {type =="sign-in" ? "welcome Back" : "join us today"}
@@ -63,7 +65,9 @@ const UserAuthForm = ({type})=>
                         </p> 
                     }
             </form>
-        </section>
+            </section>
+        </AnimationWrapper>
+        
     )
 }
 export default UserAuthForm;
